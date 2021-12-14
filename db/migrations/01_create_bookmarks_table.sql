1c4a4ea6cc3fcffa9ed9aa1a039d22f3470990ae
Titus Chang <titus@Tituss-MacBook-Air.local>
2021-12-14 15:53:17 +0000

    added instructions for setting up tables with dummy data

--- db/migrations/01_create_bookmarks_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60), title VARCHAR(60));
-
-INSERT INTO bookmarks (url, title) VALUES ('http://www.makersacademy.com', 'makers');
-INSERT INTO bookmarks (url, title) VALUES('http://www.destroyallsoftware.com', 'destroyallsoftware');
-INSERT INTO bookmarks (url, title) VALUES('http://www.google.com', 'google');
--- db/migrations/02_create_bookmark_tags_table.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE bookmark_tags(id SERIAL PRIMARY KEY, bookmark_id INTEGER REFERENCES bookmarks (id), tag_id INTEGER REFERENCES tags (id));
-
