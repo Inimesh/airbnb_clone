693755825da2a328fa6e9dd3662b9d3cf397b323
Jessica Gordon <ketchesj@gmail.com>
2021-12-16 18:58:33 +0000

    creating a booking request now gets recorded in the request SQL table

--- spec/features/add_request_spec.rb
+++ spec/features/add_request_spec.rb
@@ -3,16 +3,25 @@ require_relative './../web_helper'
 feature 'Add booking request i.e. book a space for a given date' do
   scenario 'Can click on a space to start a booking request' do
 
-    user = User.add_user(username: 'test', email: 'test@makers.co.uk', fullname: 'test user', pw: 'abc123')
-    space1 = Spaces.add_space('Cozy Cottage','Very cozy','50',"#{user.user_id}")
+    space_id = add_user_space_and_avaliability
+
     visit('/main_view')
     expect(page).to have_content 'Cozy Cottage'
     click_on 'Book'
-    expect(current_path).to be '/booking/:space_id/book'
-
-    # click_on dates checkbox to select dates
-    # click_on 'Submit booking'
-    # expect(page).to have_content 'Your booking has been requested, please wait to hear back from the host!'
+    expect(current_path).to eq "/booking/#{space_id}/book"
   end
 
+  scenario 'Can create a booking request' do
+
+    space_id = add_user_space_and_avaliability
+
+    visit("/booking/#{space_id}/book")
+
+    check '2022-01-02'
+    check '2022-01-03'
+    click_on 'Request'
+    
+    expect(current_path).to eq '/main_view'
+    expect(page).to have_content "Your request for Cozy Cottage has been submitted to the host for the following dates: 2022-01-02, 2022-01-03"
+  end
 end
